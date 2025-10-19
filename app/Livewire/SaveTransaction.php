0f125ba4ec9382e9cfed696ed640ae6defda5e5c
Pasindu Lakmal <pherath119@gmail.com>
2025-10-19 11:35:24 +0530

    missing files at transaction makes

---
+++ app/Livewire/SaveTransaction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SaveTransaction extends Component
+{
+    public function render()
+    {
+        return view('livewire.save-transaction');
+    }
+}
