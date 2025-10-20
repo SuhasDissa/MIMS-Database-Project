278393d17b250306fe1006e87e18c18f4a855496
Suhas Dissanayake <suhasdissa@gmail.com>
2025-10-20 18:16:35 +0530

    style: add bank logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,7 +1,1 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-primary text-primary-content">
-    <svg class="size-5 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M3 8L12 2L21 8V16L12 22L3 16V8Z" fill="currentColor" opacity="0.3"/>
-        <path d="M12 2V22M3 8L21 16M21 8L3 16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-        <circle cx="12" cy="12" r="3" fill="currentColor"/>
-    </svg>
-</div>
+<img src="{{ asset('logo_inline.svg') }}" alt="Bank Logo" {{ $attributes->merge(['class' => 'h-8 w-auto']) }} />
